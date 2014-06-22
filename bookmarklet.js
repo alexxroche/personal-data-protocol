d8fd98af3d4d822ec49f9c7947051fc6d0c68a9d
Alexx Roche <notice-dev@alexx.net>
2014-06-23 00:15:01 +0200

    Better bookmarklet
    Should be able to click to add bookmark

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,3 +1,4 @@
-javascript:with(document)(head.appendChild(createElement('script')).src='//raw.githubusercontent.com/alexxroche/personal-data-protocol/master/load.js?c='+Date.now())._
-
-[firefox] Content Security Policy: Directive inline script base restriction violated @ https://raw.githubusercontent.com/alexxroche/personal-data-protocol/master/load.js
+//Name: PDP
+//Location: (paste in the next line)
+javascript:with(document)(head.appendChild(createElement('script')).src='//alexxroche.github.io/personal-data-protocol/load.js?c='+Date.now())._
+// Set Tags and Keywords to access it quickly
