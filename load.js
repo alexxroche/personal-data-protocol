7d6a648cef2ff56c4fb465aebacf07f69815ddc9
Alexx Roche <notice-dev@alexx.net>
2014-06-23 16:46:37 +0200

    load.js version 0.04
    Can now reload missing overlay and replace deleted ones.

--- load.js
+++ load.js
@@ -1,6 +1,6 @@
 "undefined" == typeof PDP ? !
 (function () {
-  var a, b, c, d, e = "0.03",
+  var a, b, c, d, e = "0.04",
     f = document.createDocumentFragment(),
     g = [{
       charset: "UTF-8",
@@ -12,12 +12,12 @@
       type: "script",
       url: "//ajax.aspnetcdn.com/ajax/jQuery/jquery-2.1.0.min.js",
       async: !1,
-      nocache: !1
+      nocache: !0
     }, {
       type: "script",
       url: "//www.google.com/jsapi",
       async: !1,
-      nocache: !1
+      nocache: !0
     }, {
       charset: "UTF-8",
       type: "script",
@@ -27,5 +27,8 @@
     }];
   for (d = 0; d < g.length; d++) a = g[d], b = document.createElement(a.type), c = a.nocache ? "?v=" + e : "", "link" === a.type ? (b.rel = "stylesheet", b.href = a.url + c) : "script" === a.type && (b.src = a.url + c), b.async = a.async, f.appendChild(b);
   document.head.appendChild(f)
-})() : PDP.message("Personal Data Overlay is already loaded!", "error");
+}()) : (function() { 
+$('#PDP_overlay').length ? (function() { $('#PDP_overlay').is(":visible")  ? PDP.log('Overlay already in place', 409) : (function(){ $('#PDP_overlay').show(); PDP.log("Overlay revealied", 200);}()) }()) :
+(function() { PDP.say("Loading a fresh one", "Personal Data Overlay was deleted!"); PDP.init();})() 
+}());
 
